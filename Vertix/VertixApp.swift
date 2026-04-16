0e2b65b4cacc0e4e43ae59b43c2bf7fbea34f7f2
Felix <ffelixrichardo@gmail.com>
2026-04-16 13:52:31 +0700

    Initial Commit

---
+++ Vertix/VertixApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertixApp.swift
+//  Vertix
+//
+//  Created by student on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct VertixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
